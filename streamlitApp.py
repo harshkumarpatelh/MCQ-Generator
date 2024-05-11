9a49342a858f36d92b77dc97ceffba8bb0ddeb45
Harsh Patel <codeharsh13@gmail.com>
2024-05-11 20:50:36 +0530

    modified

--- streamlitApp.py
+++ streamlitApp.py
@@ -3,6 +3,7 @@ import json
 from langchain.callbacks import get_openai_callback
 from src.mcqgenerator.mcqgenerator import generate_evaluate_chain
 from src.mcqgenerator.utils import read_file, get_table_data
+from src.mcqgenerator.PDFGenerator import create_pdf
 import traceback
 import pandas as pd
 
@@ -12,26 +13,29 @@ with open('F:/ML/NLP/MCQ-Generator/Response.json','r') as f:
 # converting file into json format
 RESPONSE_JSON = json.dumps(RESPONSE_JSON)
 
-# creating a title for the app
-st.title("Generate MCQ with AI")
+def Inputform():
+    # creating a title for the app
+    st.title("Generate MCQ with AI")
+    with st.form("user_input"):
+        uploaded_file = st.file_uploader("Upload pdf or text")
 
-with st.form("user_input"):
-    uploaded_file = st.file_uploader("Upload pdf or text")
+        number = st.number_input("Number of MCQ",min_value = 3, max_value = 50)
+        subject = st.text_input("Enter subject", max_chars = 20,placeholder = 'Geography')
+        tone = st.text_input("Comlexity Level of Questions", max_chars = 20, placeholder = 'Simple')
+        button = st.form_submit_button("Create MCQ")
+        return [uploaded_file,number,subject,tone,button]
 
-    number = st.number_input("Number of MCQ",min_value = 3, max_value = 50)
-    subject = st.text_input("Enter subject", max_chars = 20,placeholder = 'Geography')
-    tone = st.text_input("Comlexity level of Questions", max_chars = 20, placeholder = 'Simple')
-    button = st.form_submit_button("Create MCQ")
 
-    if button and uploaded_file is not None and number and subject and tone:
+def live_generator(file,count,subject,tone,button):
+    if button and file is not None and count and subject and tone:
         with st.spinner("creating..."):
             try:
-                text = read_file(uploaded_file)
+                text = read_file(file)
                 with get_openai_callback() as cb:
                     response = generate_evaluate_chain(
                         {
                             'text':text,
-                            'number':number,
+                            'number':count,
                             'subject':subject,
                             'tone':tone,
                             'RESPONSE_JSON':RESPONSE_JSON
@@ -47,13 +51,23 @@ with st.form("user_input"):
                     if quiz is not None:
                         table_data = get_table_data(quiz)
                         if table_data is not None:
-                            df = pd.DataFrame(table_data)
+                            df = pd.DataFrame(table_data,index =None)
                             st.table(df)
+                            if st.button('Download PDF'):
+                                filename = subject + ".pdf"
+                                create_pdf(df,filename)
+                                st.success('PDF file has been downloaded!')
                             # Display the review in text box
-
                             st.text_area(label = "Review", value = response['review'])
                         else:
                             st.error("Error in table")
                     else:
                         st.write(response)
+
+
+
+(uploaded_file,number,subject,tone,button) = Inputform()
+live_generator(uploaded_file,number,subject,tone,button)
+
+
 
